91e367a4b80de254deafb6784b3454a8b8d367fd
vpspepe-usp <victor101pepe@usp.com>
2024-10-08 19:24:25 -0300

    Exercicios

---
+++ Aula_2_Python_Basico/ex0.py
@@ -0,0 +1,3 @@
+nome: str = input("Qual é o seu nome? ")
+print("Bem vindo apostador", nome, "!")
+print(f"Bem vindo apostador {nome} !") # Strings formatadas ou f-strings
---
+++ Aula_2_Python_Basico/ex1.py
@@ -0,0 +1,5 @@
+valor: float = float(input("Quanto você quer apostar? "))
+maior_que_mil: bool = valor > 1000
+
+print(f"Voce apostou {valor} reais.")
+print(f"Esse valor é maior que mil? {maior_que_mil}")
